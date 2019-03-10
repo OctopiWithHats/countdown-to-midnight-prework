6a2bdb4fefff867d44f2a593ea3c68bd17322bae
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-10 20:18:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,5 @@
 #write your code here
 
 def countdown(seconds_left)
-  while seconds_left>0
-    puts "#{seconds_left} SECOND(S)!"
-    seconds_left = seconds_left - 1
-  end
-  if seconds_left == 0
-    puts "HAPPY NEW YEAR!"
-  end
+ 
 end
